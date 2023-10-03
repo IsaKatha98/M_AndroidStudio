07830e3c8a70291c0af9ca6f209d4048ab5d4729
IsaKatha98 <113983281+IsaKatha98@users.noreply.github.com>
2023-10-03 10:20:53 +0200

    Ayuda

--- Ejercicio01_Tema01/app/src/main/java/com/example/ejercicio01_tema01/MainActivity.kt
+++ Ejercicio01_Tema01/app/src/main/java/com/example/ejercicio01_tema01/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.ejercicio01_tema01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.ejercicio01_tema01.databinding.ActivityMainBinding
+import com.example.ejercicio01_tema01.databinding.BienvenidaBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        var boton = binding.button
+
+        boton.setOnClickListener {
+            val toast = Toast.makeText(
+                applicationContext,
+                "Me has pulsado",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            val otroBinding= BienvenidaBinding.inflate(layoutInflater)
+            setContentView(otroBinding.root)
+
+        }
     }
+
 }
