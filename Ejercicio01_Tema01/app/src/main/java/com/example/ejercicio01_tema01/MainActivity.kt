c42443862ef0aec6c92ae293f0d288b7771dc7de
IsaKatha98 <isabel.loerzer@iesnervion.es>
2023-10-03 19:21:56 +0200

    Sos

--- Ejercicio01_Tema01/app/src/main/java/com/example/ejercicio01_tema01/MainActivity.kt
+++ Ejercicio01_Tema01/app/src/main/java/com/example/ejercicio01_tema01/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.ejercicio01_tema01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.concat
 import android.widget.Toast
+import android.content.Intent
 import com.example.ejercicio01_tema01.databinding.ActivityMainBinding
 import com.example.ejercicio01_tema01.databinding.BienvenidaBinding
 
@@ -18,14 +20,33 @@ class MainActivity : AppCompatActivity() {
         var boton = binding.button
 
         boton.setOnClickListener {
-            val toast = Toast.makeText(
-                applicationContext,
-                "Me has pulsado",
-                Toast.LENGTH_SHORT
-            ).show()
 
-            val otroBinding= BienvenidaBinding.inflate(layoutInflater)
-            setContentView(otroBinding.root)
+                val toast= Toast.makeText(
+                    applicationContext,
+                    "Me has pulsado",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            val oBinding= BienvenidaBinding.inflate(layoutInflater)
+            setContentView(oBinding.root)
+
+            val intent = Intent(this , Bienvenida::class.java)
+            intent.putExtra("usuario", binding.tvUser.text.toString())
+            startActivity(intent)
+           /* if(binding.tvUser.text.toString().isEmpty() || binding.tvUser.text.toString().isEmpty()){
+                Toast.makeText(applicationContext, "Error al iniciar sesión", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this , Bienvenida::class.java)
+                intent.putExtra("usuario", binding.tvUser.text.toString())
+                startActivity(intent)
+                Toast.makeText(applicationContext, "Iniciando sesión..." + binding.tvUser.text.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+*/
+
+
+
+
 
         }
     }
